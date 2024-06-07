e76513e9880a6ee602f3059528cbd149876151b4
akdfkeks <akdfkeks@gmail.com>
2024-06-08 04:20:07 +0900

    change signup route to public

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { PublicRoute } from 'src/common/decorator/public';
 import { CreateUserDto } from 'src/user/dto/create-user';
 import { UserService } from 'src/user/user.service';
 
@@ -6,6 +7,7 @@ import { UserService } from 'src/user/user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @PublicRoute()
   @Post()
   public async signup(@Body() payload: CreateUserDto) {
     return this.userService.createUser(payload);
